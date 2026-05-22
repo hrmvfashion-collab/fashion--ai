6b6aeee8fd9b86865c1ddc55ec3e66fdc9c409f6
hrmvfashion-collab <hrmvfashion@gmail.com>
2026-05-23 01:55:30 +0530

    Configure Java/Kotlin compatibility for version 17
    Added Java/Kotlin compatibility configuration for version 17.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,20 +5,42 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory
+    .dir("../../build")
+    .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// Add this new configuration block for Java/Kotlin compatibility
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.application") || 
+            project.plugins.hasPlugin("com.android.library")) {
+            
+            project.extensions.configure<com.android.build.api.dsl.CommonExtension<*, *, *, *, *, *>> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+            
+            project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+                kotlinOptions {
+                    jvmTarget = "17"
+                }
+            }
+        }
+    }
 }
